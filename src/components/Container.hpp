c457fae8d81735fb73e4e52fad3f9ae38c969340
mimvoid <mimvoid@proton.me>
2025-06-16 11:51:20 -0400

    Make Container safer

--- src/components/Container.hpp
+++ src/components/Container.hpp
@@ -4,29 +4,38 @@
 #include "../widgets/Widget.hpp"
 
 namespace katzen {
+#define STATIC_ASSERT_WIDGET(type)               \
+  static_assert(std::is_base_of_v<Widget, type>, \
+                "A katzen Container can only have derivatives of Widget.");
+
 struct Container {
   std::vector<std::unique_ptr<Widget>> children{};
 
   template <typename T>
   void pushBack(T &&child) {
+    STATIC_ASSERT_WIDGET(T);
     children.push_back(std::make_unique<T>(std::move(child)));
   }
 
   template <typename T>
   T *pushBackGet(T &&child) {
+    STATIC_ASSERT_WIDGET(T);
     children.push_back(std::make_unique<T>(std::move(child)));
     return dynamic_cast<T *>(children.back().get());
   }
 
   template <typename T, typename... Args>
   void emplaceBack(Args &&...args) {
+    STATIC_ASSERT_WIDGET(T);
     children.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
   }
 
   template <typename T, typename... Args>
   T *emplaceBackGet(Args &&...args) {
-    children.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
-    return dynamic_cast<T *>(children.back().get());
+    STATIC_ASSERT_WIDGET(T);
+    const std::unique_ptr<Widget> &ref =
+        children.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+    return dynamic_cast<T *>(ref.get());
   }
 };
 } // namespace katzen
