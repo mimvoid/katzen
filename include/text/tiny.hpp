2d863abf8546e106bbcb8c62b9a8e250bf6c7530
mimvoid <mimvoid@proton.me>
2025-12-19 22:31:35 -0500

    Add Tiny font by Matthew Welch

---
+++ include/text/tiny.hpp
@@ -0,0 +1,15 @@
+#ifndef KATZE_TEXT_TINY_HPP
+#define KATZE_TEXT_TINY_HPP
+
+#include "Font.hpp"
+
+namespace katze {
+/**
+ * Loads the Tiny font by Matthew Welch.
+ *
+ * See the source file for the font's data, description, and license.
+ */
+Font loadTinyFont();
+} // namespace katze
+
+#endif // !KATZE_TEXT_TINY_HPP
